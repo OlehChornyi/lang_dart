0f62b2043b509bd474c37f2c6596056ff3905c53
oleh <phd.chornyi@gmail.com>
2025-05-02 13:17:42 +0300

    ex-lists(6-10)

--- exercises/bin/lists.dart
+++ exercises/bin/lists.dart
@@ -4,6 +4,12 @@ void main(List<String> args) {
   print(sumList([5, 10, 15]));
   print(filterEven([1, 2, 3, 4, 5, 6]));
   print(sortList([4, 2, 7, 1])); 
+  print(reverseList(["a", "b", "c"]));
+  print(frequency(["a", "b", "a", "c", "b", "b"]));
+  print(mergeUnique([1, 2, 3], [3, 4, 5]));
+  print(areListsEqual([1, 2, 3], [1, 2, 3]));
+  print(areListsEqual([1, 2, 3], [3, 2, 1]));
+  print(flatten([1, [2, [3, 4], 5], 6]));
 }
 
 //Exercise 1: Find the Maximum Number in a List
@@ -30,4 +36,45 @@ List<int> filterEven(List<int> numbers) {
 List<int> sortList(List<int> numbers) {
   numbers.sort();
   return numbers;
+}
+
+//Exercise 6: Reverse a List
+List<T> reverseList<T>(List<T> list) {
+  return list.reversed.toList();
+}
+
+//Exercise 7: Find the Frequency of Each Element
+Map<T, int> frequency<T>(List<T> list) {
+  Map<T, int> freq = {};
+  for (var item in list) {
+    freq[item] = (freq[item] ?? 0) + 1;
+  }
+  return freq;
+}
+
+//Exercise 8: Merge Two Lists Without Duplicates
+List<T> mergeUnique<T>(List<T> list1, List<T> list2) {
+  return {...list1, ...list2}.toList();
+}
+
+//Exercise 9: Check if Two Lists Are Equal (Order Matters)
+bool areListsEqual<T>(List<T> a, List<T> b) {
+  if (a.length != b.length) return false;
+  for (int i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) return false;
+  }
+  return true;
+}
+
+//Exercise 10: Flatten a Nested List
+List flatten(List nestedList) {
+  List result = [];
+  for (var element in nestedList) {
+    if (element is List) {
+      result.addAll(flatten(element));
+    } else {
+      result.add(element);
+    }
+  }
+  return result;
 }
