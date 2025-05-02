64517f21fb68ff2c9eb12639182b0bf623e8f767
oleh <phd.chornyi@gmail.com>
2025-05-02 13:39:04 +0300

    ex-sets(6-10)

--- exercises/bin/sets.dart
+++ exercises/bin/sets.dart
@@ -4,6 +4,12 @@ void main(List<String> args) {
   print(intersection({1, 2, 3}, {2, 3, 4}));
   print(difference({1, 2, 3}, {2, 3, 4}));
   print(isSubset({1, 2, 3, 4}, {2, 3}));
+  print(convertListToSetAndBack(["a", "b", "a", "c"]));
+  print(areSetsEqual({1, 2, 3}, {3, 2, 1}));
+  print(areSetsEqual({1, 2}, {1, 2, 3}));
+  print(uniqueChars("hello"));
+  print(countUnique([1, 2, 2, 3, 4, 4]));
+  printSetElements({"apple", "banana", "cherry"});
 }
 
 //Exercise 1: Remove Duplicates from a List Using Set
@@ -29,4 +35,31 @@ Set<T> difference<T>(Set<T> a, Set<T> b) {
 //Exercise 5: Check if One Set is a Subset of Another
 bool isSubset<T>(Set<T> a, Set<T> b) {
   return a.containsAll(b);
+}
+
+//Exercise 6: Convert a List to a Set and Back
+List<T> convertListToSetAndBack<T>(List<T> list) {
+  return list.toSet().toList();
+}
+
+//Exercise 7: Check if Two Sets are Equal
+bool areSetsEqual<T>(Set<T> a, Set<T> b) {
+  return a.length == b.length && a.containsAll(b);
+}
+
+//Exercise 8: Find Unique Characters in a String
+Set<String> uniqueChars(String input) {
+  return input.split('').toSet();
+}
+
+//Exercise 9: Count Unique Elements in a List
+int countUnique<T>(List<T> list) {
+  return list.toSet().length;
+}
+
+//Exercise 10: Iterate Over a Set and Print Elements
+void printSetElements<T>(Set<T> set) {
+  for (var element in set) {
+    print(element);
+  }
 }
