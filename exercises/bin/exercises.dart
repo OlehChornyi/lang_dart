fc72ec7f809f1f975b4323a61f783b2198d540a5
oleh <phd.chornyi@gmail.com>
2025-05-02 11:35:11 +0300

    exercises

---
+++ exercises/bin/exercises.dart
@@ -0,0 +1,10 @@
+import 'package:exercises/exercises.dart' as exercises;
+
+void main(List<String> arguments) {
+  print('Hello world');
+}
+
+//STRINGS
+//LISTS
+//SETS
+//MAPS
---
+++ exercises/test/exercises_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercises/exercises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
