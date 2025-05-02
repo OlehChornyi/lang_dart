44cc6c27549d705dcae8c047aba6567725bf5e7c
oleh <phd.chornyi@gmail.com>
2025-05-02 12:31:57 +0300

    ex-strings(6-10)

--- exercises/bin/exercises.dart
+++ exercises/bin/exercises.dart
@@ -1,5 +1,3 @@
-import 'package:exercises/exercises.dart' as exercises;
-
 void main(List<String> arguments) {
   print('Hello world');
 }
--- exercises/bin/strings.dart
+++ exercises/bin/strings.dart
@@ -4,6 +4,12 @@ void main(List<String> args) {
   print(countVowels("Dart is awesome"));
   print(capitalizeWords("flutter is fun"));
   print(countCharacter("banana", "a"));
+  print(removeDigits("abc123def456")); 
+  print(replaceWord("hello world", "world", "Dart"));
+  print(longestWord("I love programming in Dart"));
+  print(isAlpha("Hello")); 
+  print(isAlpha("Hello123"));
+  print(removeDuplicates("programming")); 
 }
 
 //1. Write a function that reverses a string.
@@ -40,3 +46,53 @@ String capitalizeWords(String sentence) {
 int countCharacter(String input, String char) {
   return input.split('').where((c) => c == char).length;
 }
+
+//6. Remove all numbers from a string.
+String removeDigits(String input) {
+  return input.replaceAll(RegExp(r'\d'), '');
+}
+
+//7. Replace all instances of one substring with another.
+String replaceWord(String input, String from, String to) {
+  return input.replaceAll(from, to);
+}
+
+//8. Return the longest word from a sentence.
+String longestWord(String sentence) {
+  List<String> words = sentence.split(' ');
+  return words.reduce((a, b) => a.length > b.length ? a : b);
+}
+
+//9. Return true if a string contains only letters.
+bool isAlpha(String input) {
+  return RegExp(r'^[a-zA-Z]+$').hasMatch(input);
+}
+
+//10. Remove all duplicate characters from a string.
+String removeDuplicates(String input) {
+  Set<String> seen = {};
+  return input.split('').where((char) => seen.add(char)).join();
+}
+
+//---------------------------------------------------------------
+/* 
+
+RegExp: \s+
+\s = matches any whitespace character (spaces, tabs, newlines).
++ = matches one or more of the preceding element.
+So, \s+ matches one or more spaces/tabs/newlines in a row.
+
+RegExp: [aeiouAEIOU]
+[...] = character class: matches any one character inside the brackets.
+aeiouAEIOU = includes all lowercase and uppercase vowels.
+
+RegExp: \d
+\d = matches any single digit (0–9).
+
+RegExp: ^[a-zA-Z]+$
+^ = start of string
+[a-zA-Z] = matches any uppercase or lowercase letter.
++ = one or more of the previous character class.
+$ = end of string
+
+*/
