b07c6771588d7ba18366fb950ac9830e8f2bde77
oleh <phd.chornyi@gmail.com>
2025-05-03 13:34:36 +0300

    ex-strings-sets

--- exercises/bin/strings_sets.dart
+++ exercises/bin/strings_sets.dart
@@ -1,4 +1,33 @@
 void main(List<String> args) {
-    //Ussage of functions
+  //Ussage of functions
+  print(uniqueChars("banana"));
+  print(commonChars("hello", "world"));
+  print(removeDuplicateWords("this is is a test test")); 
+  print(countUniqueWords("Hello hello world world Dart"));
+  print(diffChars("abcdef", "bdf"));
+}
 
+//6. Find Unique Characters in a String
+Set<String> uniqueChars(String input) {
+  return input.split('').toSet();
+}
+
+//7. Find Common Characters Between Two Strings
+Set<String> commonChars(String a, String b) {
+  return a.split('').toSet().intersection(b.split('').toSet());
+}
+
+//8. Remove Duplicate Words in a Sentence
+String removeDuplicateWords(String sentence) {
+  return sentence.split(' ').toSet().join(' ');
+}
+
+//9. Count Unique Words in a Sentence
+int countUniqueWords(String sentence) {
+  return sentence.toLowerCase().split(' ').toSet().length;
+}
+
+//10. Find Characters in First String but Not in Second
+Set<String> diffChars(String a, String b) {
+  return a.split('').toSet().difference(b.split('').toSet());
 }
