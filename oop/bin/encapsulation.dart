57c342b63de715bc201deca82f26e4748194ea1f
oleh <phd.chornyi@gmail.com>
2025-05-16 15:12:03 +0300

    encapsulation 10

--- oop/bin/encapsulation.dart
+++ oop/bin/encapsulation.dart
@@ -18,6 +18,27 @@ void main(List<String> args) {
   var p1 = Product("Book", 12.99);
   print(p1.name);
   print(p1.price);
+  //-----------------------
+  var user = User();
+  user.setPassword("123"); // Too short
+  user.setPassword("secure123");
+  print(user.checkPassword("secure123")); 
+  //---------------------
+  var calc = Calculator();
+  print(calc.squareSum(2, 3));
+  //-------------------
+  var c = Counter();
+  c.value = 10;
+  print(c.value);
+  //-------------------
+  var cart = Cart();
+  cart.addItem("Apple");
+  print(cart.items);
+  // cart.items.add("Hack");
+  //-------------------
+  var c1 = Circle(5);
+  print(c1.area); // 78.5375
+  print(c1.circumference);
 }
 
 //Here are 10 Dart exercises focused on the Encapsulation principle 
@@ -79,4 +100,59 @@ class Product {
 
   String get name => _name;
   double get price => _price;
+}
+
+//6. Password Validation
+class User {
+  String _password = "";
+
+  void setPassword(String value) {
+    if (value.length >= 6) {
+      _password = value;
+    } else {
+      print("Password too short");
+    }
+  }
+
+  bool checkPassword(String value) => _password == value;
+}
+
+//7. Private Helper Method
+class Calculator {
+  int add(int a, int b) => a + b;
+  int _square(int x) => x * x;
+
+  int squareSum(int a, int b) => _square(add(a, b));
+}
+
+//8. Custom Setter with Logging
+class Counter {
+  int _value = 0;
+
+  int get value => _value;
+
+  set value(int newVal) {
+    print("Setting value to $newVal");
+    _value = newVal;
+  }
+}
+
+//9. Encapsulating Collection
+class Cart {
+  final List<String> _items = [];
+
+  List<String> get items => List.unmodifiable(_items);
+
+  void addItem(String item) => _items.add(item);
+}
+
+//10. Multiple Getters with Logic
+class Circle {
+  double _radius;
+
+  Circle(this._radius);
+
+  double get radius => _radius;
+  double get area => 3.1415 * _radius * _radius;
+  double get circumference => 2 * 3.1415 * _radius;
 }
