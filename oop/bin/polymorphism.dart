d15c6e579f5da352050f0825c7160b02916d1867
oleh <phd.chornyi@gmail.com>
2025-05-16 12:54:09 +0300

    polymorphism 10

--- oop/bin/polymorphism.dart
+++ oop/bin/polymorphism.dart
@@ -17,6 +17,28 @@ void main(List<String> args) {
   logger.log("Saving to file");
   //---------------
   printAnything(LaserPrinter());
+  //--------------
+  Employee e;
+
+  e = Manager();
+  e.doWork();
+
+  e = Developer();
+  e.doWork();
+  //--------------
+  List<Notification> methods = [Email(), SMS()];
+  for (var method in methods) {
+    method.send("Welcome!");
+  }
+  //--------------
+  Discount discount = PercentageDiscount();
+  print(discount.apply(100));
+  //--------------
+  Message m = Alert();
+  m.show();
+  //---------------
+  var t = getTransport('plane');
+  t.travel();
 }
 
 //Below are 10 Dart exercises focused on the Polymorphism principle 
@@ -86,4 +108,80 @@ class LaserPrinter extends Printer {
 
 void printAnything(Printer printer) {
   printer.printData();
+}
+
+//6. Runtime Decision on Object Type
+abstract class Employee {
+  void doWork();
+}
+
+class Manager extends Employee {
+  @override
+  void doWork() => print("Managing tasks");
+}
+
+class Developer extends Employee {
+  @override
+  void doWork() => print("Writing code");
+}
+
+//7. Polymorphism with Abstract Class and Real Use Case
+abstract class Notification {
+  void send(String message);
+}
+
+class Email extends Notification {
+  @override
+  void send(String message) => print("Sending Email: $message");
+}
+
+class SMS extends Notification {
+  @override
+  void send(String message) => print("Sending SMS: $message");
+}
+
+//8. Using Polymorphism for Extensibility
+abstract class Discount {
+  double apply(double amount);
+}
+
+class NoDiscount extends Discount {
+  @override
+  double apply(double amount) => amount;
+}
+
+class PercentageDiscount extends Discount {
+  @override
+  double apply(double amount) => amount * 0.9;
+}
+
+//9. Late Binding with Polymorphism
+class Message {
+  void show() => print("Generic message");
+}
+
+class Alert extends Message {
+  @override
+  void show() => print("This is an alert!");
+}
+
+//10. Combining Polymorphism with Factory Pattern
+abstract class Transport {
+  void travel();
+}
+
+class Car extends Transport {
+  @override
+  void travel() => print("Traveling by car");
+}
+
+class Plane extends Transport {
+  @override
+  void travel() => print("Flying on plane");
+}
+
+Transport getTransport(String type) {
+  if (type == 'car') return Car();
+  if (type == 'plane') return Plane();
+  throw Exception("Unknown transport");
 }
