c2cd08791d00c63df69d3e530f96856f79593031
oleh <phd.chornyi@gmail.com>
2025-05-17 16:54:40 +0300

    solid

---
+++ solid/bin/solid.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  print('Hello world of SOLID!');
+
+  //SOLID
+  //Single responsibility principle
+  //Open-closed principle
+  //Liskobsky substitution principle
+  //Interface segregation principle
+  //Dependency inversion principle
+}
